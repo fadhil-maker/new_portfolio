1ae4faa49c29b3f7cba3b6ffe9679377efc79921
fadhil-maker <dreamingstars2728@example.com>
2026-07-14 23:41:22 +0530

    Remove legacy Django Vercel config to allow Next.js auto-detection

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Building Project..."
-python3.12 -m pip install -r requirements.txt --break-system-packages
-python3.12 manage.py collectstatic --noinput -i input.css
-python3.12 manage.py migrate
